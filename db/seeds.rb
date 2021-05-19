a9c3ec2c14e10b3dd1721309148b3dacb668e0f5
Mathieu Jolly <mathieujolly69@gmail.com>
2021-05-19 16:12:45 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Restaurant.create(
+    name: Faker::Cannabis.brand,
+    address: Faker::Address.street_address,
+    category: Restaurant::ALL_CATEGORIES.sample
+  )
+end
